2e74efb1177231f98f82a658fb5cd8f6173375e2
Phillip Aelony <phillael@yahoo.com>
2023-01-05 17:02:09 -0500

    Update next.config, add NEXT_PUBLIC to key

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_OPEN_WEATHER_API_KEY:
+      process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY,
+    NEXT_PUBLIC_X_RAPIDAPI_KEY: process.env.NEXT_PUBLIC_X_RAPIDAPI_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
